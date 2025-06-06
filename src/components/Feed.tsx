9dd82dbc3ede2d29ed45d3c52bce0c77fb6d81ca
DonMohsen <mohsendorado@gmail.com>
2025-06-06 19:49:33 +0330

    Initial commit after cloning

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,12 +1,28 @@
+import { prisma } from "@/prisma";
 import Post from "./Post"
+import { auth } from "@clerk/nextjs/server";
 
-const Feed = () => {
+const Feed = async({userProfileId}:{userProfileId?:string}) => {
+  const{userId}=await auth()
+if(!userId){
+  return
+}
+// const followings=await prisma.follow.findMany({where:{followerId:userId},select:{followingId:true}});
+// console.log("followings:::::::",followings);
+
+ const whereConditions=userProfileId?{parentPostId:null,userId:userProfileId}:{parentPostId:null, userId:{
+  in:[userId,...((await prisma.follow.findMany({where:{followerId:userId},select:{followingId:true}})).map((following)=>following.followingId)),]
+ }}
+const posts=await prisma.post.findMany({where:whereConditions})
+  console.log(posts);
+  
   return (
     <div className=''>
+   {posts.map((post)=>(
+    <div key={post.id}>
       <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+    </div>
+   ))}
     </div>
   )
 }
