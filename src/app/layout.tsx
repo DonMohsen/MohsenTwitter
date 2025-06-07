34a1b922b529c17e6140ec9b7a433b9f0c4368e6
DonMohsen <mohsendorado@gmail.com>
2025-06-07 18:44:03 +0330

    clerk webhooks Dooooone!!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import RightBar from "@/components/RightBar";
 
 import type { Metadata } from 'next'
 import { ClerkProvider } from "@clerk/nextjs";
+import QueryProvider from "@/providers/QueryProvider";
 
 export const metadata: Metadata = {
   title: 'Lama Dev X Clone',
@@ -17,13 +18,16 @@ export default function AppLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <ClerkProvider>
+      <QueryProvider>
     
     <html lang="en">
       <body>
        {children}
       </body>
     </html>
+    </QueryProvider>
     </ClerkProvider>
   );
 }
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,30 +1,61 @@
 import { prisma } from "@/prisma";
-import Post from "./Post"
+import Post from "./Post";
 import { auth } from "@clerk/nextjs/server";
+import InfiniteFeed from "./InfiniteFeed";
 
-const Feed = async({userProfileId}:{userProfileId?:string}) => {
-  const{userId}=await auth()
-if(!userId){
-  return
-}
-// const followings=await prisma.follow.findMany({where:{followerId:userId},select:{followingId:true}});
-// console.log("followings:::::::",followings);
+const Feed = async ({ userProfileId }: { userProfileId?: string }) => {
+  const { userId } = await auth();
 
- const whereConditions=userProfileId?{parentPostId:null,userId:userProfileId}:{parentPostId:null, userId:{
-  in:[userId,...((await prisma.follow.findMany({where:{followerId:userId},select:{followingId:true}})).map((following)=>following.followingId)),]
- }}
-const posts=await prisma.post.findMany({where:whereConditions})
-  console.log(posts);
-  
+  if (!userId) return;
+
+  const whereCondition = userProfileId
+    ? { parentPostId: null, userId: userProfileId }
+    : {
+        parentPostId: null,
+        userId: {
+          in: [
+            userId,
+            ...(
+              await prisma.follow.findMany({
+                where: { followerId: userId },
+                select: { followingId: true },
+              })
+            ).map((follow) => follow.followingId),
+          ],
+        },
+      };
+
+  const postIncludeQuery = {
+    user: { select: { displayName: true, username: true, img: true } },
+    _count: { select: { likes: true, rePosts: true, comments: true } },
+    likes: { where: { userId: userId }, select: { id: true } },
+    rePosts: { where: { userId: userId }, select: { id: true } },
+    saves: { where: { userId: userId }, select: { id: true } },
+  };
+
+  const posts = await prisma.post.findMany({
+    where: whereCondition,
+    include: {
+      rePost: {
+        include: postIncludeQuery,
+      },
+      ...postIncludeQuery,
+    },
+    take: 3,
+    skip: 0,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <div className=''>
-   {posts.map((post)=>(
-    <div key={post.id}>
-      <Post/>
+    <div className="">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Post post={post} />
+        </div>
+      ))}
+      <InfiniteFeed userProfileId={userProfileId} />
     </div>
-   ))}
-    </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
