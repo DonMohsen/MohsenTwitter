a2b6d72163c4e798ce3a705faddf1fd054870daf
DonMohsen <mohsendorado@gmail.com>
2025-06-11 18:03:16 +0330

    added the clerk sign in method image

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "ik.imagekit.io",
         port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+        port: "",
+        pathname: "/oauth_google/**", // allows all images under /oauth_google/
       },
     ],
   },
